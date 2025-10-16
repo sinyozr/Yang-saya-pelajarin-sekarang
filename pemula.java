6335edd03c4ee25aed4c56138789a3cbd34ab4ec
sinyozr <sinyozr4@gmail.com>
2025-10-16 16:16:26 +0700

    Percobaan 1

---
+++ pemula.java
@@ -0,0 +1,15 @@
+public class pemula {
+
+    public static void main(String[] args) {
+        Integer x = Integer.valueOf(9);
+        Double c = Double.valueOf(5);
+        Float a = Float.valueOf("80");
+
+        Integer b = Integer.valueOf("444", 16);
+
+        System.out.println(x); // tanpa kutip
+        System.out.println(c); // tanpa kutip
+        System.out.println(a); // tanpa kutip
+        System.out.println(b); // tanpa kutip
+    }
+}
